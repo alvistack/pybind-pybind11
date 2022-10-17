f287ff501ecd783c8a3cb76388fb5df7402a148e
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2022-10-17 15:42:22 +0800

    alvistack/v2.9.1
        git clean -xdf
        tar zcvf ../python-pybind11_2.9.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-pybind11.spec ../python-pybind11_2.9.1-1.spec
        mv ../python*-pybind11*2.9.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/pybind-pybind11-2.9.1/
        rm -rf ../*pybind11*2.9.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- pybind11/commands.py
+++ pybind11/commands.py
@@ -5,17 +5,7 @@ DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_include(user=False):
-    # type: (bool) -> str
-    installed_path = os.path.join(DIR, "include")
-    source_path = os.path.join(os.path.dirname(DIR), "include")
-    return installed_path if os.path.exists(installed_path) else source_path
-
+    return '/usr/include/pybind11'
 
 def get_cmake_dir():
-    # type: () -> str
-    cmake_installed_path = os.path.join(DIR, "share", "cmake", "pybind11")
-    if os.path.exists(cmake_installed_path):
-        return cmake_installed_path
-    else:
-        msg = "pybind11 not installed, installation required to access the CMake files"
-        raise ImportError(msg)
+    return '/usr/share/cmake/pybind11'
